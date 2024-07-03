ac7d02e42e37da02e841e53098f7ebfccbb47f03
BillRob <theman@fdrsucks.com>
2024-07-03 13:52:03 -0500

    feat: Adding test app for ips

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IpsTester;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var baseUrl = configuration["baseUrl"] ?? throw new InvalidOperationException("Could not find 'baseUrl' in appSettings.");
+var bearerToken = configuration["bearerToken"] ?? throw new InvalidOperationException("Could not find 'bearerToken' in appSettings.");
+
+
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri(baseUrl);
+httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+httpClient.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "kredit-IpsTester");
+
+if (args.Length > 0 && String.Equals(args[0], "chunked", StringComparison.OrdinalIgnoreCase))
+  Console.WriteLine(args[0]);
+
+HttpRequestMessage requestMessage = null;
+
+var baseAddress = new Uri(baseUrl);
+
+var apiRequest = new ApiRequest();
+for (int i = 0; i < 2000; i++)
+{
+  apiRequest.Ssns.Add(new ApiRequest.Ssn("555115555"));
+}
+
+if (args.Length > 0 && String.Equals(args[0], "chunked", StringComparison.OrdinalIgnoreCase))
+{
+  requestMessage = new HttpRequestMessage(HttpMethod.Get, "getAccountBatch")
+  {
+    RequestUri = new Uri(baseAddress, "getAccountBatch"),
+    Content = JsonContent.Create(apiRequest),
+  };
+}
+else
+{
+  var jsonBody = JsonSerializer.Serialize(apiRequest);
+  requestMessage = new HttpRequestMessage(HttpMethod.Get, "getAccountBatch")
+  {
+    RequestUri = new Uri(baseAddress, "getAccountBatch"),
+    Content = new StringContent(jsonBody, Encoding.UTF8, "application/json"),
+  };
+}
+
+
+var httpResponse = await httpClient.SendAsync(requestMessage);
+
+var response = await httpResponse.Content.ReadAsStringAsync();
+Console.WriteLine(response);
---
+++ Request.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IpsTester;
+
+internal class ApiRequest
+{
+  [JsonPropertyName("filterFirstName")]
+  public List<FirstName> FirstNames { get; } = new();
+
+  [JsonPropertyName("filterLastName")]
+  public List<LastName> LastNames { get; } = new();
+
+  [JsonPropertyName("filterSSN")]
+  public List<Ssn> Ssns { get; } = new();
+
+  [JsonPropertyName("filterCustomerZipCodes")]
+  public List<ZipCode> ZipCodes { get; } = new();
+
+  [JsonPropertyName("limit")]
+  public int PageSize { get; } = 20;
+
+  public class FirstName(string value)
+  {
+    [JsonPropertyName("firstName")]
+    public string Value { get; } = value;
+  }
+
+  public class LastName(string value)
+  {
+    [JsonPropertyName("lastName")]
+    public string Value { get; } = value;
+  }
+
+  public class Ssn(string value)
+  {
+    [JsonPropertyName("ssn")]
+    public string Value { get; } = value;
+  }
+
+  public class ZipCode(string value)
+  {
+    [JsonPropertyName("zipCode")]
+    public string Value { get; } = value;
+  }
+}
